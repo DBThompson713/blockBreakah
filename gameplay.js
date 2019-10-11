e1614ed3d176e5a1814046921b9114b6c4dcd4ac
ironest <ironest@gmail.com>
2019-10-11 12:40:55 +1100

    Including the ball radius during the collision detection

--- gameplay.js
+++ gameplay.js
@@ -179,10 +179,10 @@ if (!isMobile()) {
         var b = bricks[c][r];
         if (b.status == 1) {
           if (
-            x > b.x &&
-            x < b.x + brickWidth &&
-            y > b.y &&
-            y < b.y + brickHeight
+            x + ballRadius > b.x &&
+            x - ballRadius < b.x + brickWidth &&
+            y + ballRadius > b.y &&
+            y - ballRadius < b.y + brickHeight
           ) {
             dy = -dy;
             b.status = 0;
